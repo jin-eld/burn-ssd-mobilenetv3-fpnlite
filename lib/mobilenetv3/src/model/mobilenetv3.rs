35a2ee461e0eb52ac48211df15b80d4e8e70fd86
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2025-01-07 11:16:28 -0500

    Fix the mappings

--- lib/mobilenetv3/src/model/mobilenetv3.rs
+++ lib/mobilenetv3/src/model/mobilenetv3.rs
@@ -56,6 +56,23 @@ impl<B: Backend> LayerType2D<B> {
     }
 }
 
+#[derive(Module, Debug)]
+pub struct Classifier<B: Backend> {
+    fc1: Linear<B>,
+    activation: Hardswish,
+    dropout: Dropout,
+    fc2: Linear<B>,
+}
+
+impl<B: Backend> Classifier<B> {
+    pub fn forward(&self, x: Tensor<B, 2>) -> Tensor<B, 2> {
+        let x = self.fc1.forward(x);
+        let x = self.activation.forward(x);
+        let x = self.dropout.forward(x);
+        self.fc2.forward(x)
+    }
+}
+
 pub enum MobileNetV3Arch {
     Large,
     Small,
@@ -202,28 +219,50 @@ fn load_weights_record<B: Backend>(
             "Could not download weights.\nError: {err}"
         ))
     })?;
+
     let load_args = LoadArgs::new(torch_weights)
         // Initial Conv and BatchNorm layers
         .with_key_remap("features\\.0\\.0\\.(.+)", "features.0.conv.$1")
-        .with_key_remap("features\\.0\\.1\\.(.+)", "features.0.norm.$1")
+        .with_key_remap("features\\.0\\.1\\.(.+)", "features.0.bn.$1")
+        // Feature layer 1 does not have an expand conv (only layers 2-15)
+        // So we only deal with the depthwise -> project convolutions
+        .with_key_remap(
+            "features\\.1\\.block\\.0\\.0\\.(.+)",
+            "features.1.depthwise_conv.conv.$1",
+        )
+        .with_key_remap(
+            "features\\.1\\.block\\.0\\.1\\.(.+)",
+            "features.1.depthwise_conv.bn.$1",
+        )
+        .with_key_remap(
+            "features\\.1\\.block\\.1\\.0\\.(.+)",
+            "features.1.project_conv.conv.$1",
+        )
+        .with_key_remap(
+            "features\\.1\\.block\\.1\\.1\\.(.+)",
+            "features.1.project_conv.bn.$1",
+        )
+        // Feature layers 2-15 below
         // Inverted Residual Blocks - Expand Conv
         .with_key_remap(
-            "features\\.(1[0-6]|[1-9])\\.block\\.0\\.0\\.(.+)",
+            "features\\.([2-9]|1[0-5])\\.block\\.0\\.0\\.(.+)",
             "features.$1.expand_conv.conv.$2",
         )
         .with_key_remap(
-            "features\\.(1[0-6]|[1-9])\\.block\\.0\\.1\\.(.+)",
-            "features.$1.expand_conv.norm.$2",
+            "features\\.([2-9]|1[0-5])\\.block\\.0\\.1\\.(.+)",
+            "features.$1.expand_conv.bn.$2",
         )
         // Inverted Residual Blocks - Depthwise Conv
         .with_key_remap(
-            "features\\.(1[0-6]|[1-9])\\.block\\.1\\.0\\.(.+)",
+            "features\\.([2-9]|1[0-5])\\.block\\.1\\.0\\.(.+)",
             "features.$1.depthwise_conv.conv.$2",
         )
         .with_key_remap(
-            "features\\.(1[0-6]|[1-9])\\.block\\.1\\.1\\.(.+)",
-            "features.$1.depthwise_conv.norm.$2",
+            "features\\.([2-9]|1[0-5])\\.block\\.1\\.1\\.(.+)",
+            "features.$1.depthwise_conv.bn.$2",
         )
+        // NOTE: block.2 remaps to project_conv when se_layer is not present, but block.3 otherwise
+        // For large, se_layer is present in features.[4,5,6,11,12,13,14,15]
         // Squeeze and Excitation layers
         .with_key_remap(
             "features\\.(1[0-6]|[1-9])\\.block\\.2\\.fc1\\.(.+)",
@@ -233,22 +272,31 @@ fn load_weights_record<B: Backend>(
             "features\\.(1[0-6]|[1-9])\\.block\\.2\\.fc2\\.(.+)",
             "features.$1.se_layer.fc2.$2",
         )
+        // When se_layer is not present, project_conv is in block.2
+        // So for features.[1,2,3,7,8,9,10] it will be in block.2
         // Inverted Residual Blocks - Project Conv
         .with_key_remap(
-            "features\\.(1[0-6]|[1-9])\\.block\\.3\\.0\\.(.+)",
+            "features\\.([1-3]|[7-9]|10)\\.block\\.2\\.0\\.(.+)",
             "features.$1.project_conv.conv.$2",
         )
         .with_key_remap(
-            "features\\.(1[0-6]|[1-9])\\.block\\.3\\.1\\.(.+)",
-            "features.$1.project_conv.norm.$2",
+            "features\\.([1-3]|[7-9]|10)\\.block\\.2\\.1\\.(.+)",
+            "features.$1.project_conv.bn.$2",
+        )
+        .with_key_remap(
+            "features\\.([4-6]|1[1-5])\\.block\\.3\\.0\\.(.+)",
+            "features.$1.project_conv.conv.$2",
+        )
+        .with_key_remap(
+            "features\\.([4-6]|1[1-5])\\.block\\.3\\.1\\.(.+)",
+            "features.$1.project_conv.bn.$2",
         )
         // Last Conv Layer
         .with_key_remap("features\\.16\\.0\\.(.+)", "features.16.conv.$1")
-        .with_key_remap("features\\.16\\.1\\.(.+)", "features.16.norm.$1")
+        .with_key_remap("features\\.16\\.1\\.(.+)", "features.16.bn.$1")
         // Classifier
-        .with_key_remap("classifier\\.0\\.(.+)", "classifier.0.$1")
-        .with_key_remap("classifier\\.3\\.(.+)", "classifier.3.$1")
-        .with_debug_print();
+        .with_key_remap("classifier\\.0\\.(.+)", "classifier.fc1.$1")
+        .with_key_remap("classifier\\.3\\.(.+)", "classifier.fc2.$1");
 
     let record = PyTorchFileRecorder::<FullPrecisionSettings>::new()
         .load(load_args, device)?;
@@ -305,17 +353,13 @@ impl MobileNetV3Config {
             .init(device),
         ));
 
-        let classifier = vec![
-            LayerType2D::Linear(
-                LinearConfig::new(lastconv_output_channels, last_channel)
-                    .init(device),
-            ),
-            LayerType2D::Activation(Activation::Hardswish(Hardswish::new())),
-            LayerType2D::Dropout(DropoutConfig::new(0.2).init()),
-            LayerType2D::Linear(
-                LinearConfig::new(last_channel, num_classes).init(device),
-            ),
-        ];
+        let classifier = Classifier {
+            fc1: LinearConfig::new(lastconv_output_channels, last_channel)
+                .init(device),
+            activation: Hardswish::new(),
+            dropout: DropoutConfig::new(0.2).init(),
+            fc2: LinearConfig::new(last_channel, num_classes).init(device),
+        };
 
         return MobileNetV3 {
             features,
@@ -378,7 +422,7 @@ impl MobileNetV3PretrainedConfig {
 pub struct MobileNetV3<B: Backend> {
     features: Vec<LayerType<B>>,
     avgpool: AdaptiveAvgPool2d,
-    classifier: Vec<LayerType2D<B>>,
+    classifier: Classifier<B>,
 }
 
 impl<B: Backend> MobileNetV3<B> {
@@ -391,12 +435,8 @@ impl<B: Backend> MobileNetV3<B> {
 
         let batch_size = x.shape().dims[0];
         let num_elements = x.shape().num_elements() / batch_size;
-        let mut reshaped = x.reshape([batch_size, num_elements]);
+        let reshaped = x.reshape([batch_size, num_elements]);
 
-        for layer in &self.classifier {
-            reshaped = layer.forward(reshaped);
-        }
-
-        return reshaped;
+        return self.classifier.forward(reshaped);
     }
 }
